70337e3c1fa823985c2a875609a226aabc8463f1
VaibhavTekkalur <tekkalurvaibhav@gmail.com>
2022-02-19 20:28:39 +0530

    smirkbot code commit

---
+++ bot.js
@@ -0,0 +1,18 @@
+const {Client , Intents} = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] } );
+
+require('dotenv').config();
+
+client.login(process.env.BOT_TOKEN);
+
+client.on('ready',() => {
+    console.log('ready');
+    client.user.setActivity('Running a test, hopefully.', { type: "PLAYING" });
+});
+
+client.on('messageCreate' ,(msg) => {
+    console.log(msg);
+    if (msg.content === '!smirk'){
+        msg.channel.send(`😏`);
+    }
+});
